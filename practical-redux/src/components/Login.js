58d589f7147b8a44189af7acbd7fa2f43f80ff2c
Krushal043 <59363395+Krushal043@users.noreply.github.com>
2022-05-16 10:26:11 +0530

    Add files via upload
    middleware

---
+++ practical-redux/src/components/Login.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../features/user";
+
+const Login = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch(login({ name: "king", age: 21, email: "king@gmail.com" }));
+        }}
+      >
+        Login
+      </button>
+    </div>
+  );
+};
+
+export default Login;
